870bbd3c216c42a66a4c29cc177603a330271c04
Илья <meowingless@mail.ru>
2023-07-20 16:15:11 +0300

    changes asked by client

--- src/lastParts.js
+++ src/lastParts.js
@@ -1,36 +1,59 @@
 const decrBtn = document.getElementById('decr-btn')
 const incrBtn = document.getElementById('incr-btn')
-const slider = document.getElementById('slider')
+const slider = document.getElementById('slider-content')
 const map = document.getElementById('map')
 let counter = 0
 let windowWidth = window.innerWidth * 46 / 100
 
-console.log(window.innerWidth);
 
 if (window.innerWidth > 1800) {
     map.width = window.innerWidth / 4
     map.height = window.innerWidth / 4
-} else if (window.innerWidth > 1500) {
-    map.width = window.innerWidth / 2.5
-    map.height = window.innerWidth / 2.5
+    windowWidth = window.innerWidth * 34 / 100
+} else if (window.innerWidth > 1000) {
+    map.width = window.innerWidth / 3
+    map.height = window.innerWidth / 3
+    windowWidth = window.innerWidth * 46 / 100
+} else if (window.innerWidth > 1000) {
+    map.width = window.innerWidth / 1.7
+    map.height = window.innerWidth / 1.7
 } else {
     map.width = window.innerWidth/1.5
     map.height = window.innerWidth/1.5
+    windowWidth = window.innerWidth * 50 / 100
 }
 
-incrBtn.addEventListener('click', () => {
-    counter += windowWidth
-    if (counter >= window.innerWidth) {
-        counter = window.innerWidth
-    }
-    slider.scrollLeft = counter
-})
+// incrBtn.addEventListener('click', () => {
+//     counter += windowWidth
+//     if (counter >= window.innerWidth) {
+//         counter = window.innerWidth
+//     }
+//     slider.scrollLeft = counter
+// })
 
+
+// decrBtn.addEventListener('click', () => {
+//     counter -= windowWidth
+//     if (counter <= 0) {
+//         counter = 0
+//     }
+//     slider.scrollLeft = counter
+// })
+
+let offset = 0
 
 decrBtn.addEventListener('click', () => {
-    counter -= windowWidth
-    if (counter <= 0) {
-        counter = 0
+    offset -= windowWidth
+    if (offset < 0) {
+        offset = window.innerWidth
     }
-    slider.scrollLeft = counter
+    slider.style.left = -offset + 'px'
+})
+
+incrBtn.addEventListener('click', () => {
+    offset += windowWidth
+    if (offset > window.innerWidth) {
+        offset = 0
+    }
+    slider.style.left = -offset + 'px'
 })
