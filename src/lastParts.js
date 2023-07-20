22d796760c97b80e4d99f7c55824078f936757a8
Илья <meowingless@mail.ru>
2023-07-20 11:07:29 +0300

    responsive changes

--- src/lastParts.js
+++ src/lastParts.js
@@ -1,25 +1,36 @@
 const decrBtn = document.getElementById('decr-btn')
 const incrBtn = document.getElementById('incr-btn')
 const slider = document.getElementById('slider')
-// const firstImg = document.getElementById('firstImg')
-// const secondImg = document.getElementById('secondImg')
-// const thirdImg = document.getElementById('thirdImg')
-// const forthImg = document.getElementById('forthImg')
+const map = document.getElementById('map')
 let counter = 0
+let windowWidth = window.innerWidth * 46 / 100
+
+console.log(window.innerWidth);
+
+if (window.innerWidth > 1800) {
+    map.width = window.innerWidth / 4
+    map.height = window.innerWidth / 4
+} else if (window.innerWidth > 1500) {
+    map.width = window.innerWidth / 2.5
+    map.height = window.innerWidth / 2.5
+} else {
+    map.width = window.innerWidth/1.5
+    map.height = window.innerWidth/1.5
+}
 
 incrBtn.addEventListener('click', () => {
-    counter += 675
-    if (counter >= 1260) {
-        counter = 1260
+    counter += windowWidth
+    if (counter >= window.innerWidth) {
+        counter = window.innerWidth
     }
     slider.scrollLeft = counter
 })
 
 
 decrBtn.addEventListener('click', () => {
-    counter -= 675
+    counter -= windowWidth
     if (counter <= 0) {
         counter = 0
     }
-    slider.scrollLeft = counter + 0.5
+    slider.scrollLeft = counter
 })
